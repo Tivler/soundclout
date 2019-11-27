867885a8df21b93ebda5e0cb31f6382f41b59f19
tivler <thomasdmckyer@gmail.com>
2019-11-27 13:32:36 -0500

    use correct env property for port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const uri = process.env.ATLAS_URI;
 const app = express()
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 //INSTANTIATE MIDDLEWARE
 
